b08baa220652890642041efabf610fc34e3b7a16
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2017-06-13 14:17:22 +0300

    added core helper

--- ckan/lib/helpers.py
+++ ckan/lib/helpers.py
@@ -2333,7 +2333,7 @@ core_helper(whtext.truncate)
 core_helper(converters.asbool)
 # Useful additions from the stdlib.
 core_helper(urlencode)
-
+core_helper(clean_html, name='clean_html')
 
 def load_plugin_helpers():
     """
